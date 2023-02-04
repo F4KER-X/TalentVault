f5dde4b78587648d0e8865d4f15941946682f789
Danny <57102573+F4KER-X@users.noreply.github.com>
2023-02-03 19:41:18 -0500

    Updated some config and added root router #11 #12
    applies changes to the root router
    Applied CORS to the routers in order to block any access.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors(corsOptions))
+
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -11,6 +13,17 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/users', require('./routes/userRoutes'))
 
 app.use('/jobs', require('./routes/jobRoutes'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: "404 ERROR: Not Found" })
+    } else {
+        res.type('txt').send('404 ERROR: Not Found')
+    }
+})
 
 
 
