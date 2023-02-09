d5d07c25ec81bf43c5b888265de6100786795bfa
laurenrigante <lrigante@hotmail.com>
2023-02-09 16:34:27 -0500

    added logo to header. began header css

--- talentvault/src/components/Header.jsx
+++ talentvault/src/components/Header.jsx
@@ -2,30 +2,32 @@
 
 import {FaSignInAlt,FaSignOutAlt,FaUser} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
+import '../index.css';
+import logo from'./logo.jpg';
 
 function Header() {
   return (
     <header className="header">
-        <div className="logo">
-            <Link to='/'>TalentVault</Link>
+    <div className='header-item-holder'>
+        <div className="Our-logo">
+            <Link to='/'>
+                <img src={logo} alt="TalentVault Logo"  width="110" height="40"></img>
+            </Link>
         </div>
-
-        <ul>
-            <li>
+        
+            <div className='login-ref'>
                 <Link to='/login'>
-                    <FaSignInAlt>Login</FaSignInAlt>
+                    Log in
                 </Link>
-            </li>
+            </div>
 
 
-            <li>
+            <div className='register-ref'>
                 <Link to='/register'>
                     <FaUser>Register</FaUser>
                 </Link>
-            </li>
-        </ul>
-
-
+            </div>
+    </div>
 
     </header>
   )
