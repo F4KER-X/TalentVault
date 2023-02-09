13e38f8ee54723b304aa29da3c47076da74de771
laurenrigante <lrigante@hotmail.com>
2023-02-09 11:36:55 -0500

    installed react icons and began header

--- talentvault/src/components/Header.jsx
+++ talentvault/src/components/Header.jsx
@@ -1,10 +1,33 @@
+//shell of a header, will need work
 
+import {FaSignInAlt,FaSignOutAlt,FaUser} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <header className="header">
+        <div className="logo">
+            <Link to='/'>TalentVault</Link>
+        </div>
+
+        <ul>
+            <li>
+                <Link to='/login'>
+                    <FaSignInAlt>Login</FaSignInAlt>
+                </Link>
+            </li>
+
+
+            <li>
+                <Link to='/register'>
+                    <FaUser>Register</FaUser>
+                </Link>
+            </li>
+        </ul>
+
+
+
+    </header>
   )
 }
 
