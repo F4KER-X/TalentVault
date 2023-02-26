81917d79a3bfffd1aa7440474ad7ca1e6058d267
Jad Hanna <jadhanna306@gmail.com>
2023-02-25 20:48:25 -0500

    new file:   package-lock.json 	new file:   package.json 	modified:   talentvault/frontend/src/pages/LandingPage.jsx

--- talentvault/frontend/src/pages/LandingPage.jsx
+++ talentvault/frontend/src/pages/LandingPage.jsx
@@ -24,10 +24,10 @@ const Landing = () => {
           </p>
           <div className="btns-main">
             <Link to="/register" className="btn1 btn-hero">
-              Login/Register
+            Register
             </Link>
-            <Link to="/jobposting" className="btn1 btn-hero">
-              Post a job
+            <Link to="/Login" className="btn1 btn-hero">
+            Login
             </Link>
           </div>
           {/* <button className="btn btn-hero">Login/Register</button> */}
