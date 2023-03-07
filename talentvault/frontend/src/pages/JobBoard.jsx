8363e3bb272216ab77e6e7037e34bf3d17690637
laurenrigante <lrigante@hotmail.com>
2023-03-07 15:14:56 -0500

    cleared about page&created job board

---
+++ talentvault/frontend/src/pages/JobBoard.jsx
@@ -0,0 +1,40 @@
+import Logo from "../components/Logo";
+import { FaBriefcase, FaRegBuilding, FaMapMarkerAlt } from "react-icons/fa";
+import Jobs from "../components/Jobs";
+import Pagination from "../components/Pagination";
+function About() {
+  return (
+    <>
+      <div>
+        <div className="top-container">
+          <Logo />
+          <h2>Explore Our Jobs!</h2>
+        </div>
+      </div>
+
+      <div className="container">
+        <div>
+          <ul className="job-list full">
+            <Jobs />
+            <li>
+              <Jobs />
+            </li>
+
+            <li>
+              <Jobs />
+            </li>
+			<l1>
+				<Jobs/>
+			</l1>
+          </ul>
+
+        <Pagination>
+
+        </Pagination>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
