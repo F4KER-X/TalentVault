59d1cae14f3bbc56a95d6486402d12b2cdfd1ef4
Adel <adelbouchatta@live.ca>
2023-03-22 11:59:41 -0400

    Added color based on request and button that links to job

--- talentvault/frontend/src/pages/Application.jsx
+++ talentvault/frontend/src/pages/Application.jsx
@@ -14,6 +14,7 @@ import { getApplicationForUser } from "../redux/features/application/application
 import Loader from "../components/Loader";
 import Applications from "../components/Applications";
 
+
 function Application() {
   //UseRedirectLoggedOutUser("/login");
   const dispatch = useDispatch();
