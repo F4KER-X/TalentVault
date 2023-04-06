7ff5c2776ac3e33b816252cb8753bc8111acfe28
despinakouli <97748664+despinakouli@users.noreply.github.com>
2023-04-06 15:42:43 -0400

    Added functionality for report bug page #80

--- talentvault/frontend/src/pages/reportbug.jsx
+++ talentvault/frontend/src/pages/reportbug.jsx
@@ -1,71 +1,96 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
+import { toast } from "react-toastify";
+import Loader from "../components/Loader";
 import Navbar from "../components/Navbar";
 import "../index.css";
 import Footer from "../components/Footer";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
 import FormRow from "../components/FormRow";
-import "../assets/styling/profile.css";
+import {validateEmail } from "../redux/features/auth/authService";
+
+//install in terminal npm i @emailjs/browser
+
+const ReportBug = () => {
+    const form = useRef();
 
 
-function ReportBug() {
+    const sendEmail = (e) => {
+      e.preventDefault();
+
+  
+      emailjs
+        .sendForm(
+        'service_0jwozuo', 
+        'template_xow7q13', 
+        form.current, 
+        'j-8eFQTP8WbH07FID'
+        )
+        .then(
+            (result) => {
+                console.log(result.text);
+                toast.success("Message sent succesfully");
+                e.target.reset();
+            }, 
+            (error) => {
+                console.log(error.text);
+            }
+        );
+    };
+
     return (
         <>
         <div>
+        {<Loader />}
         <Navbar />
+
         <h3 style={{ textAlign: "center", marginTop: "20px" }}>
             Report Bug
         </h3>
-        <div className ="form">
-            <div className="bugDetails">
+        <form ref={form} onSubmit={sendEmail}>
+        <div className = "form">
+            <div className="bugUser">
                 <h5>Submit new bug report</h5>
                 <FormRow
-                  type="text"
-                  labelText="Email"
-                  placeholder="Email"
-                  name="email"
+                    type="text"
+                    name="user_name"
+                    labelText="Name"
+                />
+
+            </div>
+
+            <div className="bugEmail">
+                <FormRow
+                    type="email"
+                    name="user_email"
+                    labelText="Email"
                 />
             </div>
 
-            <div className="bugPhoto">
-                <form onSubmit={""}>
-                    <label> Attach file </label>
-                        <input
-                         type="file"
-                         className="form-control"
-                         id="customFile"
-                         onChange={""}
-                         />
-                         <button type="submit" className="btn-bug">
-                             Upload
-                         </button>
-                </form>
-            </div>
-
-            <br/>
-            
-            <div className="bugDescription">
-                <ReactQuill
-                    theme="snow"
+            <div className="bugMessage">
+                <label className="form-label">Message</label>
+                <textarea
                     placeholder="Describe the issue in detail"
-                    className={`jobdescriptionbox ${ ""
-                        }`}
+                    name="user_message"
+                    className="form-textarea"
                 />
             </div>
 
             <div className="btndiv">
-                <button type="submit" className="btn">
-                  Submit
+                <button type="submit" className="btn" value="Send">
+                    Submit
                 </button>
             </div>
 
-
-
         </div>
+        </form>
         </div>
         <Footer/>
         </>
     )
-}
+};
+
 
 export default ReportBug;
+
+
+
