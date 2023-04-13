23e9e09c41718ed2d166bba92b1888779202699b
Ali Noureddine <ali-nd2001@outlook.com>
2023-04-13 16:22:44 -0400

    Refactored the Tests

--- talentvault/backend/test/signIn.test.js
+++ talentvault/backend/test/signIn.test.js
@@ -6,7 +6,7 @@ const request = require("supertest");
 const { deleteUserByEmail } = require("../controllers/usersController");
 chai.use(chaiHttp);
 
-describe("Auth Routes", () => {
+describe("Testing Logging in a user", () => {
   let testUserData;
   afterEach(async () => {
     if (testUserData && testUserData.email) {
@@ -14,44 +14,9 @@ describe("Auth Routes", () => {
     }
   });
 
-  describe("POST /auth/signup", () => {
-    it("should return a 400 status code if email is not provided", (done) => {
-      chai
-        .request(app)
-        .post("/auth/signup")
-        .send({ password: "testPassword", role: "recruiter" })
-        .end((err, res) => {
-          expect(res).to.have.status(400);
-          expect(res.body).to.have.property(
-            "message",
-            "Email was not provided"
-          );
-          done();
-        });
-    });
-  });
-
   describe("Auth Routes", () => {
     // Store generated user data for subsequent tests
     let testUserData;
-
-    describe("POST /auth/signup", () => {
-      it("should not register a user with an already registered email", async () => {
-        const response = await request(app).post("/auth/signup").send({
-          email: "testapplicant@example.com",
-          password: "testpassword",
-          role: "applicant",
-          firstName: "Test",
-          lastName: "Applicant",
-        });
-
-        expect(response.status).to.equal(400);
-        expect(response.body).to.have.property(
-          "message",
-          "testapplicant@example.com is already registered"
-        );
-      });
-    });
 
     describe("POST /auth/login", () => {
       it("should login a user with correct email and password", async () => {
