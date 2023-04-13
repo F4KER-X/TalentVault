6a0c60e7a63a0b3eb94f2bc5d86d2fb67af7ede9
Ali Noureddine <ali-nd2001@outlook.com>
2023-04-13 15:37:10 -0400

    Fixing a small issue with models

--- talentvault/backend/controllers/jobsController.js
+++ talentvault/backend/controllers/jobsController.js
@@ -1,5 +1,5 @@
 const Job = require("../models/Job");
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
 const Recruiter = require("../models/Recruiter");
 const Application = require("../models/Application");
 
@@ -8,27 +8,26 @@ const Application = require("../models/Application");
 // @access Private
 const getAllJobs = asyncHandler(async (req, res) => {
   //search for all jobs
-  const jobsData = await Job.find().sort({ createdAt: -1 }).lean().exec()
+  const jobsData = await Job.find().sort({ createdAt: -1 }).lean().exec();
 
   //return the jobs
-  res.status(200).json(jobsData)
-})
+  res.status(200).json(jobsData);
+});
 
 // @desc POST new job
 // @route POST /jobs
 // @access Private
 const createNewJob = asyncHandler(async (req, res) => {
-
   //get role and id from the cookie
-  const { _id, role } = req.user
+  const { _id, role } = req.user;
 
   //if not a recruiter then can't add a job
-  if (role !== 'recruiter') {
-    return res.status(401).json({ message: 'Not authorized to add a job' })
+  if (role !== "recruiter") {
+    return res.status(401).json({ message: "Not authorized to add a job" });
   }
 
   //find the recruiter to get the companyName
-  const user = await Recruiter.findOne({ userId: _id }).lean().exec()
+  const user = await Recruiter.findOne({ userId: _id }).lean().exec();
 
   //get infor from body
   const {
@@ -39,20 +38,33 @@ const createNewJob = asyncHandler(async (req, res) => {
     jobType,
     jobRequirements,
     jobLocation,
-    workType
-  } = req.body
+    workType,
+  } = req.body;
 
   //validation
-  if (!jobTitle || !maxSalary || !minSalary || !jobDescription || !jobType || !jobRequirements || !jobLocation || !workType) {
-    return res.status(400).json({ message: 'Please make sure all fields are filled out' })
+  if (
+    !jobTitle ||
+    !maxSalary ||
+    !minSalary ||
+    !jobDescription ||
+    !jobType ||
+    !jobRequirements ||
+    !jobLocation ||
+    !workType
+  ) {
+    return res
+      .status(400)
+      .json({ message: "Please make sure all fields are filled out" });
   }
 
   if (minSalary < 0 || maxSalary < 0) {
-    return res.status(400).json({ message: 'Salaries can not be negative' })
+    return res.status(400).json({ message: "Salaries can not be negative" });
   }
 
   if (minSalary >= maxSalary) {
-    return res.status(400).json({ message: 'Make sure Max and Min salaries are correct' })
+    return res
+      .status(400)
+      .json({ message: "Make sure Max and Min salaries are correct" });
   }
 
   //create job object
@@ -66,26 +78,27 @@ const createNewJob = asyncHandler(async (req, res) => {
     jobType,
     jobRequirements,
     jobLocation,
-    workType
-  }
+    workType,
+  };
 
   //add job
   const job = await Job.create(jobObject);
 
   if (!job) {
-    return res.status(400).json({ message: 'Error creating a new job, please contact us' })
+    return res
+      .status(400)
+      .json({ message: "Error creating a new job, please contact us" });
   }
 
   res.status(200).json({
     message: `${job.jobTitle} for ${job.companyName} has been added`,
-  })
-})
+  });
+});
 
 // @desc GET one job
 // @route GET /job/:id
 // @access Private
 const getOneJob = asyncHandler(async (req, res) => {
-
   try {
     const job = await Job.findById(req.params["id"]);
     if (!job) {
@@ -93,11 +106,9 @@ const getOneJob = asyncHandler(async (req, res) => {
     }
     res.json(job);
   } catch (error) {
-    return res.status(400).json({ message: 'Not a valid URL' })
+    return res.status(400).json({ message: "Not a valid URL" });
   }
-
-}
-)
+});
 
 // @desc PATCH job
 // @route PTACH /jobs/:id
@@ -108,8 +119,9 @@ const updateJob = asyncHandler(async (req, res) => {
     return res.status(404).json({ message: "Job does not exist" });
   }
 
-  const { _id } = req.user
-  if (!_id.equals(job.recruiterId)) return res.status(401).json({ message: 'Not authorized to edit this job' })
+  const { _id } = req.user;
+  if (!_id.equals(job.recruiterId))
+    return res.status(401).json({ message: "Not authorized to edit this job" });
 
   const {
     jobTitle,
@@ -120,93 +132,92 @@ const updateJob = asyncHandler(async (req, res) => {
     jobRequirements,
     jobLocation,
     status,
-    workType
+    workType,
   } = req.body;
 
-  if (jobTitle) job.jobTitle = jobTitle
+  if (jobTitle) job.jobTitle = jobTitle;
 
-  if (maxSalary) job.maxSalary = maxSalary
+  if (maxSalary) job.maxSalary = maxSalary;
 
-  if (minSalary) job.minSalary = minSalary
+  if (minSalary) job.minSalary = minSalary;
 
-  if (jobDescription) job.jobDescription = jobDescription
+  if (jobDescription) job.jobDescription = jobDescription;
 
-  if (jobType) job.jobType = jobType
+  if (jobType) job.jobType = jobType;
 
-  if (jobRequirements) job.jobRequirements = jobRequirements
+  if (jobRequirements) job.jobRequirements = jobRequirements;
 
-  if (status) job.status = status
+  if (status) job.status = status;
 
-  if (workType) job.workType = workType
+  if (workType) job.workType = workType;
 
   if (jobLocation) {
     if (jobLocation.city) {
-      job.jobLocation.city = jobLocation.city
+      job.jobLocation.city = jobLocation.city;
     }
     if (jobLocation.province) {
-      job.jobLocation.province = jobLocation.province
+      job.jobLocation.province = jobLocation.province;
     }
   }
 
-  const updatedJob = await job.save()
+  const updatedJob = await job.save();
 
   if (updatedJob) {
-    res.status(200).json({ message: 'Job updated succussfully' })
+    res.status(200).json({ message: "Job updated succussfully" });
   } else {
-    res.status(400).json({ message: 'Job update was not successful' })
+    res.status(400).json({ message: "Job update was not successful" });
   }
-
-})
+});
 
 // @desc DELETE job
 // @route DELETE /jobs/:id
 // @access Private
 const deleteJob = asyncHandler(async (req, res) => {
-
   const job = await Job.findById(req.params["id"]).exec();
   if (!job) {
     return res.status(400).json({ message: "Job posting does not exist" });
   }
-  const { _id } = req.user
+  const { _id } = req.user;
   if (!_id.equals(job.recruiterId)) {
-    return res.status(401).json({ message: 'Not authorized to delete this job' })
+    return res
+      .status(401)
+      .json({ message: "Not authorized to delete this job" });
   }
-  if (job.status === 'Open') {
-    return res.status(400).json({ message: 'Job can not be deleted, make sure it is closed' })
+  if (job.status === "Open") {
+    return res
+      .status(400)
+      .json({ message: "Job can not be deleted, make sure it is closed" });
   }
 
-
-
-  if (job.status === 'Closed') {
-    const deleteApplications = await Application.deleteMany({ jobId: req.params["id"] })
+  if (job.status === "Closed") {
+    const deleteApplications = await Application.deleteMany({
+      jobId: req.params["id"],
+    });
     if (deleteApplications) {
-      job.delete()
+      job.delete();
       res.status(200).json({
-        message: 'Job has been deleted!',
+        message: "Job has been deleted!",
       });
     }
-
   }
-
-
-})
-
+});
 
 // @desc get all jobs by user
 // @route DELETE /jobs/user-jobs
 // @access Private
 const getJobsByUser = asyncHandler(async (req, res) => {
+  const { _id, role } = req.user;
 
-  const { _id, role } = req.user
-
-  if (role === 'recruiter') {
-    const jobs = await Job.find({ recruiterId: _id }).sort({ createdAt: -1 }).lean().exec()
-    return res.status(200).json(jobs)
-
+  if (role === "recruiter") {
+    const jobs = await Job.find({ recruiterId: _id })
+      .sort({ createdAt: -1 })
+      .lean()
+      .exec();
+    return res.status(200).json(jobs);
   } else {
-    return res.status(400).json({ message: 'Not a recruiter' })
+    return res.status(400).json({ message: "Not a recruiter" });
   }
-})
+});
 
 module.exports = {
   getAllJobs,
@@ -214,5 +225,5 @@ module.exports = {
   getOneJob,
   updateJob,
   deleteJob,
-  getJobsByUser
+  getJobsByUser,
 };
