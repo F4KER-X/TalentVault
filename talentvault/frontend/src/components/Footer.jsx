9525548044c4da391df24fb7c450ce5d1967003b
Danny <57102573+F4KER-X@users.noreply.github.com>
2023-04-16 09:57:48 -0400

    Fixed issues with the footer.css and imported email.js
    Footer.css was changing the regular css of the site because it was missing more specified elements
    
    emailjs was not added to the package.json so the CI was failing.

--- talentvault/frontend/src/components/Footer.jsx
+++ talentvault/frontend/src/components/Footer.jsx
@@ -1,106 +1,106 @@
 import React from "react";
 import "./Footer.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faTwitter,faInstagram } from "@fortawesome/free-brands-svg-icons";
-import {Routes, Route, useNavigate} from 'react-router-dom';
-  
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 const Footer = () => {
-    
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const navigateToAbout = () => {
-  // 👇️ navigate to /contacts
-  navigate('/about');
-};
-
-const navigateToSignIn = () => {
+  const navigateToAbout = () => {
     // 👇️ navigate to /contacts
-    navigate('/login');
+    navigate("/about");
+  };
+
+  const navigateToSignIn = () => {
+    // 👇️ navigate to /contacts
+    navigate("/login");
   };
 
   const navigateToSignUp = () => {
     // 👇️ navigate to /contacts
-    navigate('/register');
+    navigate("/register");
   };
-
 
   //for the report bug page
   const navigateToReportBug = () => {
     // 👇️ navigate to /contacts
-    navigate('/reportbug');
+    navigate("/reportbug");
   };
 
-
-
   return (
-
-   
-<div className="footerwrapper">
-    <div className="main-footer">
+    <div className="footerwrapper">
+      <div className="main-footer">
         <div className="container">
-            <div className="intro-text">
-                <h3>TalentVault</h3>
-                <p className="intro-text2"> TalentVault is a career-services platform that helps applicants find their dream job, and helps recruiters find their dream applicant!</p>
-            </div>
-            <div className="hold-text">
-
+          <div className="intro-text">
+            <h3>TalentVault</h3>
+            <p className="intro-text2">
+              {" "}
+              TalentVault is a career-services platform that helps applicants
+              find their dream job, and helps recruiters find their dream
+              applicant!
+            </p>
+          </div>
+          <div className="hold-text">
             <div className="footer-cols">
-                <ul className="footer-links">
+              <ul className="footer-links">
                 <p className="titlefooter"> General</p>
-                    <li>
-                    <a  onClick={navigateToAbout} >About Us</a>
-                    </li>
-                    <li>
-                    <a   onClick={navigateToSignIn}>Sign In</a>
-                    </li>
-                    <li>
-                    <a  onClick={navigateToSignUp}>Sign Up</a>
-                    </li>
-                </ul>
+                <li>
+                  <a onClick={navigateToAbout}>About Us</a>
+                </li>
+                <li>
+                  <a onClick={navigateToSignIn}>Sign In</a>
+                </li>
+                <li>
+                  <a onClick={navigateToSignUp}>Sign Up</a>
+                </li>
+              </ul>
             </div>
-            
-           
+
             <div className="footer-cols">
-            <ul className="footer-links">
-            <p className="titlefooter">Help</p>
-                    <li>
-                    <a  onClick={navigateToReportBug}>Report Bug</a>
-                    </li>
-                    <li>
-                    <a >FAQ</a>
-                    </li>
-                    <li>
-                    <a >Leave a Review</a>
-                    </li>
-                </ul>
-           </div>
+              <ul className="footer-links">
+                <p className="titlefooter">Help</p>
+                <li>
+                  <a onClick={navigateToReportBug}>Report Bug</a>
+                </li>
+                <li>
+                  <a>FAQ</a>
+                </li>
+                <li>
+                  <a>Leave a Review</a>
+                </li>
+              </ul>
+            </div>
 
-           <div className="footer-cols">
-                <ul className="footer-links">
+            <div className="footer-cols">
+              <ul className="footer-links">
                 <p className="titlefooter"> Follow Our Socials</p>
-                    <li className="faicon">
-                    <FontAwesomeIcon icon={faTwitter} />
-                    </li>
-                    <li className="faicon">
-                    <FontAwesomeIcon icon={faInstagram} />
-                    </li>
-                    <li className="faicon">
-                    <FontAwesomeIcon icon={faFacebook} />
-                    </li>
-                </ul>
+                <li className="faicon">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </li>
+                <li className="faicon">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </li>
+                <li className="faicon">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </li>
+              </ul>
             </div>
-        </div>
+          </div>
 
-            <div className="row-footer">
-                <p className="intro-text2">
-                    &copy;{new Date().getFullYear()} TalentVault Corporation | Terms of Services | Privacy
-                </p>
-            </div>
+          <div className="row-footer">
+            <p className="intro-text2">
+              &copy;{new Date().getFullYear()} TalentVault Corporation | Terms
+              of Services | Privacy
+            </p>
+          </div>
         </div>
+      </div>
     </div>
- </div>
   );
 };
 export default Footer;
